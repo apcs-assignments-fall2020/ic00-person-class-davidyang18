90e9a5717451fc70be5344bf0d3bd4809c9a5dac
davidyang18 <2817822828@qq.com>
2020-10-27 16:23:26 +0000

    in-class Person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,28 @@
 public class Person {
-    
+    public String firstname;
+    public String lastname;
+
+    // Constructor
+    public Person(String a, String b) {
+        firstname = a;
+        lastname = b;
+    }
+
+    public void doSomething() {
+        System.out.println("I'm watching Netflix.");
+    }
+    public String toString() {
+        return this.firstname + "/" + this.lastname;
+    }
+
+    public String getFirstName() {
+        return this.firstname;
+    }
+    public void setFirstName(String str) {
+        if (str.trim().length() != 0) {
+            this.firstname = str;
+        }
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person person = new Person("David", "Yang");
+        person.doSomething();
+        System.out.println(person);
     }
 }
